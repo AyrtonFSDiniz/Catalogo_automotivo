7e7ff8f0e97522df3d12359bd611a4d15243fcec
AyrtonFSDiniz <ayrtondefreitassilva@gmail.com>
2021-10-11 23:20:00 -0300

    Introduction Sequelize

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const path = require("path");
+require("dotenv").config();
 
 const app = express();
-const port = 3000;
+const db = require("./model/database");
+const port = process.env.PORT; /*|| 3000*/
+const Carros = require("./model/carros");
 
 app.set("view engine", "ejs");
-app.use(express.static(path.join(__dirname, "public")));
-app.use(express.urlencoded());
+app.use(express.static(path.join(__dirname, "views/public")));
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.render("index");
@@ -28,6 +31,12 @@ app.get("/edicao", (req, res) => {
   res.render("edicao");
 });
 
+app.get("/carros", async (req, res) => {
+  const carros = await Carros.findAll();
+  res.json(carros);
+});
+
+db.conectado();
 app.listen(port, () =>
   console.log(`Servidor rodando em http://localhost:${port}`)
 );
---
+++ model/carros.js
@@ -0,0 +1,54 @@
+const { Sequelize, DataTypes } = require("sequelize");
+const database = require("./database");
+
+const Carros = database.sequelize.define(
+  "carros",
+  {
+    id: {
+      type: Sequelize.INTEGER,
+      autoIncrement: true,
+      allowNull: false,
+      primaryKey: true,
+    },
+    nome: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    marca: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    ano: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+    },
+    cilindrada: {
+      type: Sequelize.DOUBLE,
+      allowNull: false,
+    },
+    potencia: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+    },
+    peso: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+    },
+    numerocilindros: {
+      type: Sequelize.DOUBLE,
+      allowNull: false,
+    },
+    imagem_url: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    freezeTabName: true,
+    timestamps: false,
+    createdAt: false,
+    updatedAt: false,
+  }
+);
+
+module.exports = Carros;
